b3a9d25bdadd381ffd948585d874178f0cb5fbc5
mayarmtalghamdi-maker <mayar.mt.alghamdi@gmail.com>
2026-06-30 23:53:24 +0300

    Update spotify-auth.js

--- spotify-auth.js
+++ spotify-auth.js
@@ -1,1 +1,14 @@
+const CLIENT_ID = "dede0cc513fc419ab9701c2b2a3b2d0f";
+const REDIRECT_URI = "https://btsfm.netlify.app/";
+const SCOPES = [
+  "user-read-private",
+  "user-read-email"
+].join(" ");
 
+const loginButton = document.getElementById("loginBtn");
+
+if (loginButton) {
+  loginButton.addEventListener("click", () => {
+    alert("Spotify Login Starting...");
+  });
+}
